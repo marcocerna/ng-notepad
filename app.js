54ebe571a655811a997be9b296a6c2ef463662c1
Marco <marco.a.cerna@gmail.com>
2014-03-11 17:33:33 -0700

    Added skeleton of functions for directive and factory

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ angular.module('noteApp', [])
     restrict: 'AE',
     scope: {},
     link: function(scope, elem, attrs) {
+      scope.restore = function() {};
 
+      scope.openEditor = function() {};
+
+      scope.saveNote = function() {};
     },
     templateUrl: 'template.html'
   }
 })
 
 .factory('notesFactory', function() {
+  return {
+    put: function(note) {};
 
+    get: function(index) {};
+
+    getAll: function() {};
+  }
 })
